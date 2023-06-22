770c5d22e637ec0b46dcf231ca1a8666c3771db4
Sanjaynd <134504165+Sanjaynd@users.noreply.github.com>
2023-06-22 10:59:47 +0530

    Add files via upload

---
+++ challenge_01.py
@@ -0,0 +1,13 @@
+#challenge 01
+class Point:
+    def __init__(self,x,y,z):
+        self.x=x
+        self.y=y
+        self.z=z
+        
+    def sqsum(self):
+        return self.x**2+self.y**2+self.z**2
+
+point=Point(1,3,5)
+sqrsum=point.sqsum()
+print(sqrsum)
